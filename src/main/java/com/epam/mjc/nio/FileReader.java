09e6439d468ca8826cece0e048c704360e7959f5
volbog <volbog93@gmail.com>
2023-05-21 13:33:27 +0200

    Finish all tasks

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (BufferedReader buffer = Files.newBufferedReader(Path.of(file.getAbsolutePath()))) {
+            Map<String, String> map = new HashMap<>();
+            buffer.lines().forEach(line -> {
+                String[] keyValue = line.split(": ");
+                if (keyValue.length == 2) {
+                    String key = keyValue[0].trim();
+                    String value = keyValue[1].trim();
+                    map.put(key, value);
+                }
+            });
+            profile.setName(map.get("Name"));
+            profile.setAge(Integer.parseInt(map.get("Age")));
+            profile.setEmail(map.get("Email"));
+            profile.setPhone(Long.parseLong(map.get("Phone")));
+        } catch (IOException e) {
+            return null;
+        }
+        return profile;
     }
 }
